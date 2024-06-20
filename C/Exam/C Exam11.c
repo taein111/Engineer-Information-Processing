7244e6e26282701e833ad6d0e7e1fd89353ff2a0
taein111 <taein4546@naver.com>
2024-06-20 21:51:33 +0900

    C work

---
+++ C/Exam/C Exam11.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int Soojebi(int num){
+    int i;
+    for(i=2; i<num; i++){
+        if(num % i == 0)
+        return 0;
+    }
+    return 1;
+}
+
+int main(){
+    int num=10, cnt =0, i;
+    for(i=2; i<num; i++)
+    cnt += Soojebi(i);
+    printf("%d\n", cnt);
+}
